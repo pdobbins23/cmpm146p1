c166ce91ae0b0fa7d8af0bb9189ae8805f093e29
AshtonTankGallistel <123789343+AshtonTankGallistel@users.noreply.github.com>
2024-06-28 13:29:58 -0700

    added temp fix until mesh bug gets resolved

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -7,20 +7,23 @@ def find_path(source_point, destination_point, mesh):
     destination_box = ();
     for rect in mesh["boxes"]:
         if source_point[0] >= rect[0] and source_point[0] <= rect[1] and source_point[1] >= rect[2] and source_point[1] <= rect[3]:
-            print(source_point, rect)
-            source_box = rect
-            if(destination_box != ()):
-                print("both found!")
-                break
+            if(source_box == ()):
+                print(source_point, rect)
+                source_box = rect
+                if(destination_box != ()):
+                    print("both found!")
+                    break
         if destination_point[0] >= rect[0] and destination_point[0] <= rect[1] and destination_point[1] >= rect[2] and destination_point[1] <= rect[3]:
-            print(destination_point, rect)
-            destination_box = rect
-            if(source_box != ()):
-                print("both found!")
-                break
+            if(destination_box == ()):
+                print(destination_point, rect)
+                destination_box = rect
+                if(source_box != ()):
+                    print("both found!")
+                    break
 
     path.append(source_point)
     path.append(destination_point)
     # boxes{};
     
     return path, boxes.keys()
+    

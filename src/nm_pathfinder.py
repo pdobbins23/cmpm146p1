ead8036cc4765b769cb5b44da457749fc799e1b3
AshtonTankGallistel <123789343+AshtonTankGallistel@users.noreply.github.com>
2024-06-29 17:08:30 -0700

    Completed step 2, added BFS search

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -1,3 +1,5 @@
+from queue import Queue
+
 def find_path(source_point, destination_point, mesh):
     path = []
     boxes = {}
@@ -21,15 +23,59 @@ def find_path(source_point, destination_point, mesh):
                     print("both found!")
                     break
 
-    path.append(source_point)
-    boxes[source_box] = ();
+    #If there's no start/end point/box
+    if source_box == () or destination_box == ():
+        print("No path!")
+        return path, boxes.keys()
 
-    # use mesh["adj"][box_info] to find adj boxes, box_info acts as the key and is the coords
-    # breadth first search example given in wednesday lecture, should likely use as example when coding
-    # currently at step 2 of suggested workflow, "Implement the simplest complete search algorithm you can"
-    
-    path.append(destination_point)
-    boxes[destination_box] = ();
+    boxPath = BFS(source_box, destination_box, mesh["adj"])
+
+    print(boxPath)
+
+    if boxPath == None:
+        print("No path!")
+    else:
+        for box in boxPath:
+            path.append(midpoint(box))
+            boxes[box] = ()
     
     return path, boxes.keys()
-    
+
+
+# Code written by Amit Patel (https://www.redblobgames.com/pathfinding/a-star/introduction.html)
+# Performs BFS path search
+def BFS(source_box, destination_box, adjDict):
+    toVisit = Queue()
+    toVisit.put(source_box)
+    cameFrom = dict()
+    cameFrom[source_box] = None
+
+    while not toVisit.empty():
+        current = toVisit.get()
+        if(current == destination_box):
+            break
+        if(current not in adjDict):
+            break
+        for next in adjDict[current]:
+            if next not in cameFrom:
+                toVisit.put(next)
+                cameFrom[next] = current
+
+    # If not valid path, return none
+    if not destination_box in cameFrom:
+        print("return None for boxPath")
+        return None
+
+    path = [destination_box]
+    cur = destination_box
+
+    while cameFrom[cur] != None:
+        cur = cameFrom[cur]
+        path.insert(0, cur)
+
+    return path
+
+# helper, returns midpoint of box
+def midpoint(box): #x1,x2,y1,y2
+    print(box)
+    return ((box[0] + box[1]) / 2, (box[2] + box[3]) / 2)

fe1a01b3377448f1ff814d927ad7ce2642e205ac
AshtonTankGallistel <123789343+AshtonTankGallistel@users.noreply.github.com>
2024-06-30 15:06:20 -0700

    Finished part 3, updated lines to be legal. Also, foudn critical bug in teacher's code and accommodated for it

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -8,14 +8,14 @@ def find_path(source_point, destination_point, mesh):
     source_box = ();
     destination_box = ();
     for rect in mesh["boxes"]:
-        if source_point[0] >= rect[0] and source_point[0] <= rect[1] and source_point[1] >= rect[2] and source_point[1] <= rect[3]:
+        if source_point[1] >= rect[2] and source_point[1] <= rect[3] and source_point[0] >= rect[0] and source_point[0] <= rect[1]:
             if(source_box == ()):
                 print(source_point, rect)
                 source_box = rect
                 if(destination_box != ()):
                     print("both found!")
                     break
-        if destination_point[0] >= rect[0] and destination_point[0] <= rect[1] and destination_point[1] >= rect[2] and destination_point[1] <= rect[3]:
+        if destination_point[1] >= rect[2] and destination_point[1] <= rect[3] and destination_point[0] >= rect[0] and destination_point[0] <= rect[1]:
             if(destination_box == ()):
                 print(destination_point, rect)
                 destination_box = rect
@@ -35,11 +35,20 @@ def find_path(source_point, destination_point, mesh):
     if boxPath == None:
         print("No path!")
     else:
+        path.append(source_point)
+        curPoint = source_point
+        lastBox = None
         for box in boxPath:
-            path.append(midpoint(box))
             boxes[box] = ()
+            # For every box after the first,
+            # determine a line between the last and the current
+            if not lastBox == None:
+                curPoint = calcPointLocation(lastBox,curPoint,box)
+                path.append(curPoint)
+            lastBox = box
+        path.append(destination_point)
 
-    # On step 3, fixing midpoints
+    
     
     return path, boxes.keys()
 
@@ -78,6 +87,32 @@ def BFS(source_box, destination_box, adjDict):
     return path
 
 # helper, returns midpoint of box
-def midpoint(box): #x1,x2,y1,y2
+def midpoint(box): #y1,y2,x1,x2
     print(box)
-    return ((box[0] + box[1]) / 2, (box[2] + box[3]) / 2)
+    return ((box[2] + box[3]) / 2, (box[0] + box[1]) / 2)
+
+#helper, determines where point2 should be
+def calcPointLocation(start_box, start_point, end_box):
+    y,x = 0,0
+    #if end_box to north
+    if(start_box[0] == end_box[1]): # s.y1 == e.y2
+        print("north")
+        y = end_box[1]
+        x = min(max(end_box[2], start_point[1]), end_box[3])
+    #else if to east
+    elif(start_box[3] == end_box[2]): # s.x2 == e.x1
+        print("east")
+        x = end_box[2]
+        y = min(max(end_box[0], start_point[0]), end_box[1])
+    #else if to south
+    elif(start_box[1] == end_box[0]): # s.y2 == e.y3
+        print("south")
+        y = end_box[0]
+        x = min(max(end_box[2], start_point[1]), end_box[3])
+    #else (west)
+    elif(start_box[2] == end_box[3]): # s.x1 == e.x2
+        print("west")
+        x = end_box[3]
+        y = min(max(end_box[0], start_point[0]), end_box[1])
+    print(str(start_point) + " to " + str(y) + "," + str(x))
+    return (y,x)
